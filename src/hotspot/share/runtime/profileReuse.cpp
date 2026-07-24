5920126d1c3016ce6dfb2fa65608c836ae3e0ace
Parnav Harinathan <parnavh@outlook.com>
2026-07-24 22:19:36 +0100

    feat: added compile level and refactored

--- src/hotspot/share/runtime/profileReuse.cpp
+++ src/hotspot/share/runtime/profileReuse.cpp
@@ -1,6 +1,7 @@
 #include "runtime/profileReuse.hpp"
 #include "classfile/classLoaderData.hpp"
 #include "classfile/classLoaderDataGraph.hpp"
+#include "code/nmethod.hpp"
 #include "oops/instanceKlass.hpp"
 #include "oops/klass.hpp"
 #include "oops/method.hpp"
@@ -20,8 +21,14 @@ void ProfileReuse::capture_all() {
     return;
   }
 
-  fprintf(_capture_file, "className\tmethodName\tdescriptor\tinvocationCount\tb"
-                         "ackedgeCount\thasMethodData\tmdoRecords\n");
+  /*
+   * count means different things in different scenarios:
+   *    METHOD: invocation count
+   *    RECEIVER: overflow count - when there are more than 2 types at the site
+   *              more accurately type overflow count
+   */
+  fprintf(_capture_file, "recordType\tclassName\tmethodName\tdescriptor\tbci\tt"
+                         "ag\tcount\textra\n");
 
   ClassLoaderDataGraph::classes_do(&ProfileReuse::collect_klass);
 
@@ -50,6 +57,8 @@ void ProfileReuse::collect_klass(Klass *k) {
     return;
   }
 
+  const char *class_name = ik->name()->as_C_string();
+
   Array<Method *> *methods = ik->methods();
   for (int i = 0; i < methods->length(); i++) {
     Method *m = methods->at(i);
@@ -61,70 +70,75 @@ void ProfileReuse::collect_klass(Klass *k) {
 
     int invocation_count = mcs->invocation_counter()->count();
     int backedge_count = mcs->backedge_counter()->count();
-    bool has_mdo = (m->method_data() != nullptr);
 
-    if (invocation_count == 0) {
+    bool has_compiled_code = (m->code() != nullptr);
+    int comp_level = has_compiled_code ? m->code()->comp_level() : 0;
+
+    bool has_mdo_check = (m->method_data() != nullptr);
+
+    if (invocation_count == 0 && backedge_count == 0 && !has_mdo_check &&
+        !has_compiled_code) {
       continue;
     }
 
-    stringStream mdo_stream;
-    bool first_record = true;
+    const char *method_name = m->name()->as_C_string();
+    const char *descriptor = m->signature()->as_C_string();
 
-    if (has_mdo) {
-      MethodData *mdo = m->method_data();
-      ProfileData *pdata = mdo->first_data();
+    fprintf(_capture_file,
+            "METHOD\t%s\t%s\t%s\t-\t-\t%d\tbackedge=%d,compile_level=%d\n",
+            class_name, method_name, descriptor, invocation_count,
+            backedge_count, comp_level);
 
-      while (mdo->is_valid(pdata)) {
-        int tag = pdata->tag();
+    MethodData *mdo = m->method_data();
+    if (mdo == nullptr) {
+      continue;
+    }
 
-        if (tag == DataLayout::parameters_type_data_tag) {
-          // Deferred for now
-        } else if (tag == DataLayout::receiver_type_data_tag ||
-                   tag == DataLayout::virtual_call_data_tag) {
-          ReceiverTypeData *rdata = static_cast<ReceiverTypeData *>(pdata);
+    ProfileData *pdata = mdo->first_data();
+    while (mdo->is_valid(pdata)) {
+      int tag = pdata->tag();
+      int bci = pdata->bci();
 
-          if (!first_record)
-            mdo_stream.print(";");
+      if (tag == DataLayout::parameters_type_data_tag) {
+        // Deferred for now
+      } else if (tag == DataLayout::receiver_type_data_tag ||
+                 tag == DataLayout::virtual_call_data_tag) {
+        ReceiverTypeData *rdata = static_cast<ReceiverTypeData *>(pdata);
 
-          first_record = false;
-
-          mdo_stream.print("%d:%d:%d:", pdata->bci(), tag, rdata->count());
-
-          uint row_limit = ReceiverTypeData::row_limit();
-          for (uint row = 0; row < row_limit; row++) {
-            Klass *recv = rdata->receiver(row);
-            uint count = rdata->receiver_count(row);
-            if (row > 0)
-              mdo_stream.print(",");
-            if (recv != nullptr) {
-              mdo_stream.print("%s=%u", recv->name()->as_C_string(), count);
-            } else {
-              mdo_stream.print("null=0");
-            }
-          }
-
-        } else {
-          // generic counter-cell dump for BranchData/CounterData/JumpData/etc.
-          if (!first_record)
-            mdo_stream.print(";");
-          first_record = false;
-
-          mdo_stream.print("%d:%d:", pdata->bci(), tag);
-
-          int cells = pdata->cell_count();
-          for (int c = 0; c < cells; c++) {
-            mdo_stream.print("%s%ld", c == 0 ? "" : ",",
-                             (long)pdata->intptr_at_public(c));
+        stringStream extra;
+        uint row_limit = ReceiverTypeData::row_limit();
+        for (uint row = 0; row < row_limit; row++) {
+          Klass *recv = rdata->receiver(row);
+          uint count = rdata->receiver_count(row);
+          if (row > 0)
+            extra.print("|");
+          if (recv != nullptr) {
+            extra.print("%s:%u", recv->name()->as_C_string(), count);
+          } else {
+            extra.print("null:0");
           }
         }
 
-        pdata = mdo->next_data(pdata);
-      }
-    }
+        fprintf(_capture_file, "RECEIVER\t%s\t%s\t%s\t%d\t%d\t%d\t%s\n",
+                class_name, method_name, descriptor, bci, tag, rdata->count(),
+                extra.as_string());
 
-    fprintf(_capture_file, "%s\t%s\t%s\t%d\t%d\t%d\t%s\n",
-            ik->name()->as_C_string(), m->name()->as_C_string(),
-            m->signature()->as_C_string(), invocation_count, backedge_count,
-            has_mdo ? 1 : 0, mdo_stream.as_string());
+      } else {
+        // Generic counter-cell dump for BranchData/CounterData/JumpData/etc.
+        stringStream extra;
+        int cells = pdata->cell_count();
+        for (int c = 0; c < cells; c++) {
+          if (c > 0)
+            extra.print(",");
+          extra.print("%ld", (long)pdata->intptr_at_public(c));
+        }
+
+        fprintf(_capture_file, "COUNTER\t%s\t%s\t%s\t%d\t%d\t-\t%s\n",
+                class_name, method_name, descriptor, bci, tag,
+                extra.as_string());
+      }
+
+      pdata = mdo->next_data(pdata);
+    }
   }
 }
